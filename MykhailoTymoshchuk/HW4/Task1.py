70ba65a46977dc4d6a47f11c08775dd70b0ecf1b
Mykhailo Tymoshchuk <simplysex_13@icloud.com>
2024-03-27 20:20:19 +0200

    added comments

--- MykhailoTymoshchuk/HW4/Task1.py
+++ MykhailoTymoshchuk/HW4/Task1.py
@@ -1,6 +1,7 @@
-celsius = float(input("Enter temperature in celsius: "))
+celsius = float(input("Enter temperature in celsius: ")) #Поле вводу користувачем потрібної температури
 
-fahrenheit = (celsius * 1.8) + 32
+fahrenheit = (celsius * 1.8) + 32 #формула для конвертації Цельсій в Фаренгейти
 
-print('%.2f Celsius is equivalnt to: %.2f Fahrenheit'
+print('%.2f Celsius is equivalnt to: %.2f Fahrenheit' #Виводимо отриману інформацію. %.2f - це позначка ,яка вказує на те
+      #що повинно бути виведено з плаваючою точкою(десяткове число)
       %(celsius,fahrenheit))
