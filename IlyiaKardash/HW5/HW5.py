d97c5bfe547bbe611c85de5781f45bea72b9d6e1
Ilia Kardash <iliyakardash@gmail.com>
2024-03-27 23:46:25 +0200

    HW5 was added

---
+++ IlyiaKardash/HW5/HW5.py
@@ -0,0 +1,47 @@
+"""5.1"""
+
+
+# num_list = [2, 11, 90, 88, 20, 74, 91, 3, 5]
+# new_num_list = []
+
+# for item in num_list:
+#     new_num_list.append(float(item))
+# else:
+#     f'Invalid data'
+
+# new_num_list.sort()
+# print(new_num_list)
+
+
+"""5.2"""
+
+# user_input = int(input("Enter your number: "))
+
+# s1 = 0
+# s2 = 1
+
+# if user_input <= s1:
+#     print("Invalid number. Try again")
+# else:
+#     print("Fibonacci sequence:")
+#     for item in range(user_input):
+#         print(s1)
+#         s3 = s1 + s2
+#         s1 = s2
+#         s2 = s3
+
+
+"""5.3"""
+
+
+# user_input = int(input("Enter your number: "))
+
+# a1 = 1
+
+# while user_input > 0:
+#     for item in range(a1, user_input+1):
+#         a1 = a1 * item
+#     print(a1)
+#     break
+# else:
+#     print("Invalid number")
