f64948dac69680cb86a111bb0d19914c2d46c9e7
Oleh Hanhal <hanhaloleg59@gmail.com>
2024-03-27 13:50:54 +0200

    added HW4

---
+++ HanhalOleh/HW4/Temperature_converter.py
@@ -0,0 +1,7 @@
+celsius = int(input('enter the temperature in celsius: '))
+
+if celsius > -273.15:
+    farenheit = (celsius * 9/5) + 32
+    print(str(celsius) + "°C is equivalent to", str(round(farenheit)) +"°F")
+else:
+    print('error: temperature below absolute zero')
