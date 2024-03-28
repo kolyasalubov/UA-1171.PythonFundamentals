a2013cf97988406179e5e25fa3c577ed20a0e43c
Yurii Yevpak <132289443+YuriiYevpak@users.noreply.github.com>
2024-03-28 22:41:05 +0200

    Add files via upload

---
+++ YuriiYevpakHW5.py
@@ -0,0 +1,20 @@
+# Task 1
+ls = [1, 2, 3, 4, 5]
+
+result_ls = []
+for num in ls:
+    result_ls.append(float(num))
+
+print(ls)
+print(result_ls)
+
+
+
+Task 2
+n = int(input("n: "))
+fib = [0, 1]
+
+while fib[-1] + fib[-2] <= n:
+    f = fib[-1] + fib[-2]
+    fib.append(f)
+    print(f, end=" ")
