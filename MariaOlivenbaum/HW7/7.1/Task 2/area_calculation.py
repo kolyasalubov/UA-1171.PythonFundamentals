54239feca707b5ba6f92670aecb2a9ad8f6fed78
Maria Olivenbaum <malia.olivenbaum@gmail.com>
2024-04-08 01:51:05 +0200

    The first part of homework for Lesson 7 was done

---
+++ MariaOlivenbaum/HW7/7.1/Task 2/area_calculation.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def comp_area_of_rectangle(length, breadth):
+    return length * breadth
+
+def comp_area_of_triangle(base, height):
+    return 0.5 * base * height
+
+def comp_area_of_circle(radius):
+    return round(pi * radius**2, 2)
---
+++ MariaOlivenbaum/HW7/7.1/Task 3/main.py
@@ -0,0 +1,4 @@
+from functions import split_str_by_char
+
+user_string = input("Please enter your string: ")
+print(split_str_by_char(user_string))
