639cd3007a2d2c62ab3f73381da296331b7dd360
Ilia Kardash <iliyakardash@gmail.com>
2024-04-10 19:08:17 +0300

    HW9 added

--- IlyiaKardash/HW9/Task_1.py
+++ IlyiaKardash/HW9/Task_1.py
@@ -1,13 +1,13 @@
 from random import randint
 
 number_of_tries = 0
+rand_number = randint(1, 100)
 
 while number_of_tries < 10:
-    rand_number = randint(1, 100)
     number_of_tries += 1
     user_input = int(input("Enter your number: "))
-    if user_input <= 0:
-        print('Invalid number. Try positive ones.')
+    if user_input not in range(1, 101):
+        print('Invalid number. Try numbers in a range from 1 to 100 only')
     elif user_input > rand_number:
         print('Your number is greater.')
     elif user_input < rand_number:
@@ -15,5 +15,6 @@ while number_of_tries < 10:
     else:
         user_input == rand_number
         print('Congrats! You guessed the number!')
+        break
 else:
     print('You\'ve run out of tries.')
