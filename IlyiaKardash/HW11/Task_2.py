3e725b8ba02ae56c1d908df670a28e485156ea04
Ilia Kardash <iliyakardash@gmail.com>
2024-04-21 19:19:22 +0300

    HW 11 added

---
+++ IlyiaKardash/HW11/Task_2.py
@@ -0,0 +1,16 @@
+days_of_week = {1: "Monday", 2: "Tuesday", 3: "Wednesday",
+                4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
+
+
+def weekday():
+    user_inp = int(input("Enter a number: "))
+    try:
+        if user_inp > 7:
+            raise ValueError('Enter a number between 1 and 7 only')
+        if user_inp in days_of_week:
+            return days_of_week.get(user_inp)
+    except ValueError as e:
+        return f'Error. {e}'
+
+
+print(weekday())
